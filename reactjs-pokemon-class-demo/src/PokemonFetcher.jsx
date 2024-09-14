2869bf638aa4bb74bccf76279f3d9934ea5c3ee3
Alex <alex.holder@bigfootds.com>
2024-09-14 11:27:20 +1000

    feat: pokemon data from api on load

--- reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -10,8 +10,23 @@ export default class PokemonFetcher extends React.Component {
 		super(props);
 
 		this.state = {
-			pokemonList: ['pikachu', 'squirtle', 'mewtwo']
+			pokemonList: []
 		}
+	}
+
+	// runs automatically ONCE when the page first loads 
+	async componentDidMount(){
+		// generate a random number for random Pokemon number 
+		let randomNumber = Math.floor(Math.random() * 1025) + 1;
+
+		// pass random Pokemon number into fetch request
+		let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+		let data = await response.json();
+
+		// set fetch response data into state 
+		this.setState({pokemonList: [data]});
+
+		console.log("PokemonFetcher first load on the page");
 	}
 
 	render(){
@@ -19,9 +34,9 @@ export default class PokemonFetcher extends React.Component {
 			<div>
 				<h1>Pokemon Data</h1>
 				{this.state.pokemonList.map(pokemon => {
-					return <PokemonCard name={pokemon} />
+					return <PokemonCard name={pokemon.name} />
 				})}
-				
+
 				<button onClick={() => {
 					this.setState({pokemonList: []});
 				}}>
