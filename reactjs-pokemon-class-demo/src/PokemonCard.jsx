ac0a22275baac1aea3f72678506f725200c0e3a1
Alex <alex.holder@bigfootds.com>
2024-09-14 11:54:33 +1000

    feat: converted card to class and implement lifecycle methods

--- reactjs-pokemon-class-demo/src/PokemonCard.jsx
+++ reactjs-pokemon-class-demo/src/PokemonCard.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
 
 
-export default function PokemonCard (props){
-	return(
-		<h1>
-			{props.name}
-		</h1>
-	)
+export default class PokemonCard extends React.Component{
+	
+	constructor(props){
+		super(props);
+
+		this.state = {
+			name: this.props.name
+		}
+	}
+
+	componentWillUnmount(){
+		console.log("Card for Pokemon is being unloaded: " + this.state.name);
+	}
+
+	render(){
+		return(
+			<h1>
+				{this.state.name}
+			</h1>
+		)
+	}
+	
 }
--- reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -13,6 +13,7 @@ export default class PokemonFetcher extends React.Component {
 			pokemonList: [],
 			// pokemon1: null,
 			// pokemon2: null
+			userJwt: null
 		}
 	}
 
@@ -45,12 +46,22 @@ export default class PokemonFetcher extends React.Component {
 		console.log("PokemonFetcher first load on the page");
 	}
 
+	componentDidUpdate(){
+		console.log("Page has rendered! The current state is: " + this.state.pokemonList);
+
+		if (this.state.userJwt){
+			console.log("User is logged in!");
+		} else {
+			console.log("User is logged out");
+		}
+	}
+
 	render(){
 		return (
 			<div>
 				<h1>Pokemon Data</h1>
-				{this.state.pokemonList.map(pokemon => {
-					return <PokemonCard name={pokemon.name} />
+				{this.state.pokemonList.map((pokemon, index) => {
+					return <PokemonCard key={pokemon.name + index} name={pokemon.name} />
 				})}
 
 				<button onClick={() => {
